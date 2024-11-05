22b2711ffa5634ffe5d7a6f5fe05a3ae0826acb5
Lance Lu <77723432+Lancelottery@users.noreply.github.com>
2024-11-05 23:53:06 +0000

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,10 +4,10 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="ac215-hw2-lance"
+export GCP_PROJECT="vernal-house-435720-s0"
 export GCP_ZONE="us-central1-a"
-export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
+export GOOGLE_APPLICATION_CREDENTIALS="../secrets/data-service-account.json"
 
 
 echo "Building image"
